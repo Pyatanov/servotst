8a3330c681c11104e6c1157a25a1f16f402a22ee
Pyatanov <124527808+Pyatanov@users.noreply.github.com>
2023-12-01 19:57:23 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+startbit.startbit_Init()
+let angle = 1
+startbit.setPWMServo(startbit.startbit_servorange.range2, 1, angle, 500)
+basic.pause(500)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        angle = angle + 20
+        if (angle >= 270) {
+            angle = 270
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        angle = angle - 20
+        if (angle <= 0) {
+            angle = 0
+        }
+    }
+    startbit.setPWMServo(startbit.startbit_servorange.range2, 1, angle, 20)
+})
